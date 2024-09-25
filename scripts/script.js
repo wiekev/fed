c35cfa19eceaa01ab266fd3ad1ab0e54f4a69af7
wiekev <152178391+wiekev@users.noreply.github.com>
2024-09-25 21:10:08 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,22 @@
 // JavaScript Document
 console.log("hi");
 
+let menuKnop = document.querySelector("header ul li:last-of-type svg:first-of-type");
+let sluitKnop = document.querySelector("header ul li:last-of-type svg:last-of-type")
+
+menuKnop.onclick = openMenu;
+sluitKnop.onclick = sluitMenu;
+
+function openMenu(){
+    let nav = document.querySelector("nav");
+    nav.classList.add("toon-menu");
+    document.querySelector(".verdwijn").style.display = "block";
+    document.querySelector(".weg").style.display = "none";
+}
+
+function sluitMenu(){
+    let nav = document.querySelector("nav");
+    nav.classList.remove("toon-menu");
+    document.querySelector(".verdwijn").style.display = "none";
+    document.querySelector(".weg").style.display = "block";
+}
