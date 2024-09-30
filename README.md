f0232ce45e674b7e9c38cddeb602cf15c4e61791
wiekev <152178391+wiekev@users.noreply.github.com>
2024-09-30 15:17:14 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -185,7 +185,7 @@ De afbeeldingen hebben niet allemaal een alt dus het zegt gewoon afbeeldingen en
   ### Stand van zaken
   hier dit ging goed & dit was lastig (neem ook screenshots op van delen van je website en code)
 gaat wel goed moet wel nog veel responsiv maken maar voor de rest ben ik klaar.
-
+ 
   ### Agenda voor meeting
   samen met je groepje opstellen
 
@@ -194,7 +194,9 @@ gaat wel goed moet wel nog veel responsiv maken maar voor de rest ben ik klaar.
   | dit bespreken  | en dit             | en ik dit    | en dan ik dat    |
   | en dat ook nog | dit als er tijd is | nog een punt | dit wil ik zeker |
   | ...            | ...                | ...          | ...              |
-moet ik ook alle interactie doen als ik responsive doe, zoals een formelier?
+Moet ik ook alle interactie doen als ik responsive doe, zoals een formelier?
+Moet ik ook een foto achter de foto platsen voor scaduw, of is dat te klein en is het niet erg als ik niet alles heb?
+
 
   ### Verslag van meeting
   hier na afloop snel de uitkomsten van de meeting vastleggen
--- scripts/script.js
+++ scripts/script.js
@@ -2,21 +2,40 @@
 console.log("hi");
 
 let menuKnop = document.querySelector("header ul li:last-of-type svg:first-of-type");
-let sluitKnop = document.querySelector("header ul li:last-of-type svg:last-of-type")
+let sluitKnop = document.querySelector("header ul li:last-of-type svg:last-of-type");
+let nav = document.querySelector("nav");
+let body = document.querySelector("body");
 
 menuKnop.onclick = openMenu;
 sluitKnop.onclick = sluitMenu;
 
 function openMenu(){
-    let nav = document.querySelector("nav");
     nav.classList.add("toon-menu");
     document.querySelector(".verdwijn").style.display = "block";
     document.querySelector(".weg").style.display = "none";
+    body.classList.add("noScroll")
 }
 
 function sluitMenu(){
-    let nav = document.querySelector("nav");
     nav.classList.remove("toon-menu");
     document.querySelector(".verdwijn").style.display = "none";
     document.querySelector(".weg").style.display = "block";
+    body.classList.remove("noScroll")
 }
+
+
+let laatsteScroll = 0;
+const header = document.querySelector("body > article")
+
+window.addEventListener("scroll", () => {
+    let huidigescroll = window.pageYOffset;
+    
+
+    if ( huidigescroll > laatsteScroll) {
+       header.style.transform = "translateY(-100%)";
+    } else {
+        header.style.transform = "translateY(0)";
+    }
+
+    laatsteScroll = huidigescroll;
+});
